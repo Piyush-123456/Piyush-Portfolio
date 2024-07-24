bfc9aaa1a48e4b3b2ba2e814de74e201ddbbdb33
PIYUSH PANCHABHAI <piyushpanchabhai200@gmail.com>
2024-07-24 20:26:12 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Piyush_Panchabhai_😁",
+  description: "Porfolio => Piyush Panchabhai",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}> 
+        <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+      </body>
+    </html>
+  );
+}
